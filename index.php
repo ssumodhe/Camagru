a72fe2bf046a824f4b5693a94aec94402265f1a0
Safaiya SUMODHEE <ssumodhe@e2r10p15.42.fr>
2017-08-21 18:19:40 +0200

    settings webcam

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+   <!-- hello -->
+    <head>
+        <title> My Camagru !</title>
+        <meta charset="utf-8">
+      <!--  <script src="index.js"></script> -->
+        <style type="text/css">
+            .id_video{
+                
+                background-color: burlywood;
+                border-width: 1vh;
+                border-color: black;
+                border-style: double;
+                
+                }
+            #videoElement{
+                width: 500px;
+                height: 375px;
+                }
+            
+        </style>
+    </head>
+    
+    
+    <body>
+        <div class="id_video">
+        <!--     <video id="video"></video>
+            <canvas id="canvas"></canvas> -->
+            <video autoplay="true" id="videoElement"></video>
+            <br/>
+            <button id="takepic">Prendre la photo</button>
+
+        </div>
+        
+        
+    </body>
+    
+    
+</html>
